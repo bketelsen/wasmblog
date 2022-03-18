4f9d8ca5350b45b2559bca457cc2fb77fbb349f2
Brian Ketelsen <bketelsen@gmail.com>
2022-03-18 10:08:38 -0400

    trying new git compare

--- contrib/update.sh
+++ contrib/update.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cd /opt/spin/wasmblog && git pull && chown -R spin:spin .
-systemctl restart bartholomew.service
-sleep 10
-systemctl restart bartholomew2.service
+cd /opt/spin/wasmblog || exit
+old_version="$(git rev-parse --short HEAD)"
+git pull && chown -R spin:spin .
+new_version="$(git rev-parse --short HEAD)"
+
+if [ "$old_version" = "$new_version" ]; then
+    echo "No changes"
+    return
+else
+    echo "Changes detected, restarting services"
+    systemctl restart bartholomew.service
+    sleep 10
+    systemctl restart bartholomew2.service
+fi
