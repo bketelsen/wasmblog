6fcba5868ba910f1b6f844cb252340d6e21ac858
Brian Ketelsen <bketelsen@gmail.com>
2022-03-18 05:19:03 -0400

    update installer script

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ systemctl daemon-reload
 systemctl enable bartholomew.service
 systemctl start bartholomew.service
 systemctl status bartholomew.service
+
+cp ./contrib/systemd/update-bart.service /etc/systemd/system/
+cp ./contrib/systemd/update-bart.timer /etc/systemd/system/
+
+systemctl enable update-bart.timer
+
+systemctl restart update-bart.timer
